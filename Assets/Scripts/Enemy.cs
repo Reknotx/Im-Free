aa94650e2c4f784cbdb1b4c059e7fda80fdad6e0
Reknotx <chaseoconnor3@comcast.net>
2021-02-10 17:47:33 -0800

    enemy ai POC done

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+
+    /// <summary> Flag to check if the enemy has ever seen the player. </summary>
+    public bool SeenPlayer { get; set; } = false;
+
+    public void FixedUpdate()
+    {
+        if (!SeenPlayer) return;
+
+        Flee();
+    }
+
+    public void Flee()
+    {
+        Vector3 heading = Player.Instance.transform.position - transform.position;
+
+        //heading.y = 1f;
+
+        heading *= -1;
+
+        GetComponent<Rigidbody>().MovePosition(transform.position + heading.normalized * 5f * Time.deltaTime);
+    }
+
+
+}
