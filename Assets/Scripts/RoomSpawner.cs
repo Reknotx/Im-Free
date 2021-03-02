f54357ee73541abb0ded761e0202c3f0b4bf144b
Reknotx <chaseoconnor3@comcast.net>
2021-03-02 14:02:45 -0800

    setting up scriptable obj next

--- Assets/Scripts/RoomSpawner.cs
+++ Assets/Scripts/RoomSpawner.cs
@@ -7,7 +7,8 @@ public class RoomSpawner : MonoBehaviour
     public List<GameObject> spawnableRooms = new List<GameObject>();
     public List<GameObject> spawnableCorridors = new List<GameObject>();
 
-    public List<GameObject> spawnOrder = new List<GameObject>();
+
+    public List<GameObject> spawnedObjs = new List<GameObject>();
 
 
     enum Spawning
@@ -21,77 +22,86 @@ public class RoomSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log(spawnableRooms.Count + spawnableCorridors.Count);
         SpawnLevel();
 
-        Debug.Log(spawnableRooms.Count + spawnableCorridors.Count);
     }
 
 
     void SpawnLevel()
     {
+        List<GameObject> spawnOrder = new List<GameObject>();
+        
         DetermineOrder();
 
         SpawnRooms();
-    }
 
-
-    void DetermineOrder()
-    {
-        Debug.Log("Determining order");
-        while (spawnOrder.Count < 4)
+        void DetermineOrder()
         {
-            bool findingSpawn = true;
-            while (findingSpawn)
+            Debug.Log("Determining order");
+            while (spawnOrder.Count < (spawnableCorridors.Count + spawnableRooms.Count))
             {
-
-                switch (spawning)
+                bool findingSpawn = true;
+                while (findingSpawn)
                 {
-                    case Spawning.room:
-                        int roomIndex = Random.Range(0, spawnableRooms.Count);
 
-                        if (!spawnOrder.Contains(spawnableRooms[roomIndex]))
-                        {
-                            spawnOrder.Add(spawnableRooms[roomIndex]);
-                            spawning = Spawning.corridor;
-                            findingSpawn = false;
-                        }
+                    switch (spawning)
+                    {
+                        case Spawning.room:
+                            int roomIndex = Random.Range(0, spawnableRooms.Count);
 
-                        break;
+                            if (!spawnOrder.Contains(spawnableRooms[roomIndex]))
+                            {
+                                spawnOrder.Add(spawnableRooms[roomIndex]);
+                                spawning = Spawning.corridor;
+                                findingSpawn = false;
+                            }
 
-                    case Spawning.corridor:
-                        int corridorIndex = Random.Range(0, spawnableCorridors.Count);
+                            break;
 
-                        if (!spawnOrder.Contains(spawnableRooms[corridorIndex]))
-                        {
-                            spawnOrder.Add(spawnableRooms[corridorIndex]);
-                            spawning = Spawning.room;
-                            findingSpawn = false;
-                        }
-                        break;
+                        case Spawning.corridor:
+                            int corridorIndex = Random.Range(0, spawnableCorridors.Count);
+
+                            if (!spawnOrder.Contains(spawnableCorridors[corridorIndex]))
+                            {
+                                spawnOrder.Add(spawnableCorridors[corridorIndex]);
+                                spawning = Spawning.room;
+                                findingSpawn = false;
+                            }
+                            break;
+                    }
                 }
             }
         }
-    }
 
-    void SpawnRooms()
-    {
-        Debug.Log("Spawning rooms");
-        for (int i = 0; i < spawnOrder.Count; i++)
+        void SpawnRooms()
         {
-            GameObject spawnable = (GameObject)spawnOrder[i];
+            Debug.Log("Spawning rooms");
 
-            GameObject spawned = Instantiate(spawnable);
+            foreach (GameObject item in spawnOrder)
+            {
+                GameObject spawned = Instantiate(item);
+                spawnedObjs.Add(spawned);
+            }
 
-            Vector3 spawnPos;
+            for (int i = 0; i < spawnedObjs.Count; i++)
+            {
+                GameObject spawned = (GameObject)spawnedObjs[i];
 
-            if (i == 0) spawnPos = Vector3.zero;
-            else spawnPos = spawnOrder[i - 1].transform.Find("Room End").position;
+                Vector3 spawnPos;
 
-            spawned.transform.position = spawnPos;
+                if (i == 0) spawnPos = Vector3.zero; 
+                else spawnPos = spawnedObjs[i - 1].transform.Find("Room End").position;
 
-            spawnOrder.Add(spawned);
+                spawned.transform.position = spawnPos;
+
+            }
         }
+
     }
+
+
+    
 
     // Update is called once per frame
     void Update()
