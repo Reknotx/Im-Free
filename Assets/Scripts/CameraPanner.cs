0c19959046f0ab27bcea64fcb4667a5811a3e1d4
Reknotx <chaseoconnor3@comcast.net>
2021-03-02 10:07:37 -0800

    saving stuff

--- Assets/Scripts/CameraPanner.cs
+++ Assets/Scripts/CameraPanner.cs
@@ -24,6 +24,12 @@ public class CameraPanner : MonoBehaviour
 
     void CameraMovement()
     {
+        RaycastHit info;
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (!Physics.Raycast(ray, out info, 1000f, 1 << 8)) return;
+
         float maxScreenPoint = 0.8f;
 
         //Vector3 mousePos = Input.mousePosition * maxScreenPoint + new Vector3(Screen.width, 0f, Screen.height) * ((1f - maxScreenPoint) * 0.5f);
@@ -39,9 +45,15 @@ public class CameraPanner : MonoBehaviour
         //Vector3 position = (target.position + Camera.main.ScreenToWorldPoint(Input.mousePosition)) / 2f;
         Vector3 camPos = Camera.main.ScreenToWorldPoint(mousePos);
 
+        //if (Vector3.Distance(camPos, target.position) < 5f) return;
+
         Vector3 position = new Vector3(target.position.x + camPos.x, 0f, target.position.z + camPos.z) / 2f;
         Vector3 destination = new Vector3(position.x, 0, position.z);
-        transform.localPosition = Vector3.SmoothDamp(transform.localPosition, destination, ref velocity, dampTime);
+        transform.parent.localPosition = Vector3.SmoothDamp(transform.parent.localPosition, destination, ref velocity, dampTime);
+
+        ///take out 45 degree rot val
+        ///calculate
+        ///put 45 back in
     }
 
 }
---
+++ Assets/Scripts/RoomSpawner.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+    public List<GameObject> spawnableRooms = new List<GameObject>();
+    public List<GameObject> spawnableCorridors = new List<GameObject>();
+
+    public List<GameObject> spawnOrder = new List<GameObject>();
+
+
+    enum Spawning
+    {
+        room,
+        corridor
+    }
+
+    private Spawning spawning = Spawning.room;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        while(spawnOrder.Count < (spawnableRooms.Count + spawnableCorridors.Count))
+        {
+            bool findingSpawn = true;
+            while(findingSpawn)
+            {
+                
+                switch (spawning)
+                {
+                    case Spawning.room:
+                        int roomIndex = Random.Range(0, spawnableRooms.Count);
+
+                        if (!spawnOrder.Contains(spawnableRooms[roomIndex]))
+                        {
+                            spawnOrder.Add(spawnableRooms[roomIndex]);
+                            spawning = Spawning.corridor;
+                            findingSpawn = false;
+                        }
+
+                        break;
+                    
+                    case Spawning.corridor:
+                        int corridorIndex = Random.Range(0, spawnableCorridors.Count);
+
+                        if (!spawnOrder.Contains(spawnableRooms[corridorIndex]))
+                        {
+                            spawnOrder.Add(spawnableRooms[corridorIndex]);
+                            spawning = Spawning.room;
+                            findingSpawn = false;
+                        }
+                        break;
+                    
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
