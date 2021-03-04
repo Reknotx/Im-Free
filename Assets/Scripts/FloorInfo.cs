7ccdce805587219c7bf6f2c06588b3c56e6de5bd
Reknotx <chaseoconnor3@comcast.net>
2021-03-04 10:06:14 -0800

    changing names

--- Assets/Scripts/FloorInfo.cs
+++ Assets/Scripts/FloorInfo.cs
@@ -13,14 +13,31 @@ public struct LevelStuff
     /// <summary> The list of room prefabs for this level theme. </summary>
     public List<GameObject> rooms;
 
-    [Tooltip("The list of corridor prefabs for this level theme.")]
-    /// <summary> The list of corridor prefabs for this level theme. </summary>
-    public List<GameObject> corridors;
+    [Tooltip("The list of room prefabs that are used in corridors for this level theme.")]
+    /// <summary> The list of room prefabs that are used in corridors for this level theme. </summary>
+    public List<GameObject> corridorRooms;
+
+    [HideInInspector] public List<GameObject> corridors;
 }
 
 
 [CreateAssetMenu]
 public class FloorInfo : ScriptableObject
 {
+    [Tooltip("The amount of rooms and corridors that are spawned.")]
+    /// <summary> The amount of rooms and corridors that are spawned. </summary>
+    public int RCCount = 3;
+
     public List<LevelStuff> floorInfo = new List<LevelStuff>();
+
+
+    public void GenerateCorridors(int index)
+    {
+        for (int i = 0; i < RCCount; i++)
+        {
+
+        }
+    }
+
+
 }
