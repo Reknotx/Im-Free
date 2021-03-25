6d82a7b55063da7ebaa9aaba51c3ff8bd0cc5d1e
Reknotx <chaseoconnor3@comcast.net>
2021-03-24 20:37:37 -0700

    i'm just dumb

--- Assets/Scripts/CameraPanner.cs
+++ Assets/Scripts/CameraPanner.cs
@@ -6,54 +6,107 @@ public class CameraPanner : MonoBehaviour
 {
     Vector3 startPos;
 
-    Transform target;
 
-    float dampTime = 0.3f;
+    public float panningSpeed = 5f;
     private Vector3 velocity = Vector3.zero;
+    public Vector3 offset;
+    private Vector3 baseOffset;
+
+    public float horizNeutZone = 5f, vertNeutZone = 5f;
+    public float offsetLimitX = 1f, offsetLimitZ = 1f;
+
+    private float origOffsetLimitX, origOffsetLimitZ;
+
+    public GameObject playerObj;
 
     private void Start()
     {
+        if (playerObj == null)
+        {
+            playerObj = Player.Instance.gameObject;
+        }
+
         startPos = transform.position;
-        target = Player.Instance.transform;
+        baseOffset = offset = transform.localPosition - Player.Instance.playerTrans.TransformPoint(playerObj.transform.position);
+
+        origOffsetLimitX = offsetLimitX;
+        origOffsetLimitZ = offsetLimitZ;
     }
 
     private void Update()
     {
-        CameraMovement();
+        transform.position = playerObj.transform.position + Player.Instance.playerTrans.TransformPoint(offset);
+
+
+        panCamera();
+        //CameraMovement();
+
+        offset.x = Mathf.Clamp(offset.x, baseOffset.x - offsetLimitX, baseOffset.x + offsetLimitX);
+        offset.z = Mathf.Clamp(offset.z, baseOffset.z - offsetLimitZ, baseOffset.z + offsetLimitZ);
     }
+
+    private void panCamera()
+    {
+
+
+        float xModifier = Mathf.Abs(Input.mousePosition.x - getScrnFrac(true, 2f)) / getScrnFrac(true, 2f);
+        float zModifier = Mathf.Abs(Input.mousePosition.y - getScrnFrac(false, 2f)) / getScrnFrac(false, 2f);
+
+
+        if (Input.mousePosition.x < getScrnFrac(true, 2f) - getScrnFrac(true, horizNeutZone))
+        {
+            offset.x -= xModifier * panningSpeed * Time.deltaTime;
+
+        }
+        else if (Input.mousePosition.x > getScrnFrac(true, 2f) + getScrnFrac(true, horizNeutZone))
+        {
+            offset.x += xModifier * panningSpeed * Time.deltaTime;
+
+        }
+        else
+        {
+            //offset.x = Mathf.SmoothStep(offset.x, baseOffset.x, panningSpeed * 3f * Time.deltaTime);
+            offset.x = Mathf.Lerp(offset.x, baseOffset.x, panningSpeed * 0.5f * Time.deltaTime);
+        }
+
+        if (Input.mousePosition.y < getScrnFrac(false, 2f) - getScrnFrac(false, vertNeutZone))
+        {
+            offset.z -= zModifier * panningSpeed * Time.deltaTime;
+        }
+        else if (Input.mousePosition.y > getScrnFrac(false, 2f) + getScrnFrac(false, vertNeutZone))
+        {
+            offset.z += zModifier * panningSpeed * Time.deltaTime;
+        }
+        else
+        {
+            //offset.z = Mathf.SmoothStep(offset.z, baseOffset.z, panningSpeed * 3f * Time.deltaTime);
+            offset.z = Mathf.Lerp(offset.z, baseOffset.z, panningSpeed * 0.4f * Time.deltaTime);
+        }
+    }
+
 
     void CameraMovement()
     {
-        RaycastHit info;
 
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        if (!Physics.Raycast(ray, out info, 1000f, 1 << 8)) return;
-
-        float maxScreenPoint = 0.8f;
-
-        //Vector3 mousePos = Input.mousePosition * maxScreenPoint + new Vector3(Screen.width, 0f, Screen.height) * ((1f - maxScreenPoint) * 0.5f);
-
-        //Vector3 position = (Player.Instance.transform.localPosition + Camera.main.ScreenToWorldPoint(mousePos)) / 2f;
-
-        //Vector3 destination = new Vector3(position.x, 0, position.z);
-
-        //transform.localPosition = Vector3.SmoothDamp(transform.localPosition, destination, ref velocity, dampTime);
-
-
-        Vector3 mousePos = Input.mousePosition * maxScreenPoint + new Vector3(Screen.width, 0f, Screen.height) * ((1f - maxScreenPoint) * 0.5f);
-        //Vector3 position = (target.position + Camera.main.ScreenToWorldPoint(Input.mousePosition)) / 2f;
-        Vector3 camPos = Camera.main.ScreenToWorldPoint(mousePos);
-
-        //if (Vector3.Distance(camPos, target.position) < 5f) return;
-
-        Vector3 position = new Vector3(target.position.x + camPos.x, 0f, target.position.z + camPos.z) / 2f;
-        Vector3 destination = new Vector3(position.x, 0, position.z);
-        transform.parent.localPosition = Vector3.SmoothDamp(transform.parent.localPosition, destination, ref velocity, dampTime);
 
         ///take out 45 degree rot val
         ///calculate
         ///put 45 back in
+    }
+
+    private float getScrnFrac(bool widthOrHeight, float amountToDivide)
+    {
+        float result;
+        if (widthOrHeight)
+        {
+            result = Screen.currentResolution.width / amountToDivide;
+        }
+        else
+        {
+            result = Screen.currentResolution.height / amountToDivide;
+        }
+
+        return result;
     }
 
 }
