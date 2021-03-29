c5546bb1b60c3027ce8af6ab9465f958cc1a47d7
Chase O'Connor <chaseoconnor3@comcast.net>
2021-03-29 13:31:36 -0700

    got bullets to stack on player

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    bool moving = true;
 
     // Update is called once per frame
     void Update()
     {
+        if (!moving) return;
         GetComponent<Rigidbody>().MovePosition(transform.position + transform.forward * Time.deltaTime * 15f);
 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 11)
+        {
+            moving = false;
+            transform.parent = Player.Instance.transform;
+            Player.Instance.TranqDartStack++;
+            GetComponent<SphereCollider>().enabled = false;
+            GetComponent<Rigidbody>().isKinematic = true;
+        }
     }
 }
