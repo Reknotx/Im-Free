b31aa03b64fa4e3100626700a5d39d324501a86f
Chase O'Connor <chaseoconnor3@comcast.net>
2021-04-13 10:02:22 -0700

    saaaaaaaaaaaaaaaaaaaaaaaave

--- Assets/Scripts/UI/LeaderBoard.cs
+++ Assets/Scripts/UI/LeaderBoard.cs
@@ -12,7 +12,23 @@ public class LeaderBoard : MonoBehaviour
 
     public void CheckForNewHighScore()
     {
+        for (int i = 0; i < 5; i++)
+        {
+            switch (i)
+            {
+                case 1:
+                    break;
 
+                case 2:
+                    break;
+
+
+
+
+                default:
+                    break;
+            }
+        }
     }
 
     public void AddScoreToLeaderBoard()
