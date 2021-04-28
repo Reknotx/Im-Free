205f1370d80b60a7e72918c4b6be153563fa2a48
Reknotx <chaseoconnor3@comcast.net>
2021-04-28 13:51:51 -0700

    saving in audio, pause menu set up complete

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+#endif
+
+        Application.Quit();
+    }
+}
