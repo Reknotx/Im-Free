5642676b300df8753fb3c09a826807cfd65d3154
Reknotx <chaseoconnor3@comcast.net>
2021-05-13 11:04:27 -0700

    FINAL SAVE

--- Assets/Scripts/Destroy.cs
+++ Assets/Scripts/Destroy.cs
@@ -7,9 +7,16 @@ public class Destroy : MonoBehaviour
     public AudioClip musicClip;
     public AudioClip sfxClip;
 
+    public GameObject location;
+    public CanvasGroup locationFade;
+
     void Start()
     {
         AmbientAudioManager.Instance.ChangeMusic(musicClip);
+
+        location.gameObject.SetActive(true);
+
+        LeanTween.alphaCanvas(locationFade, 1f, 1f).setOnComplete(StartDelay);
     }
 
     public void Delete()
@@ -17,4 +24,16 @@ public class Destroy : MonoBehaviour
         Destroy(this.gameObject);
     }
 
+    void StartDelay()
+    {
+        StartCoroutine(Delay());
+    }
+
+    IEnumerator Delay()
+    {
+        yield return new WaitForSeconds(3f);
+
+
+        LeanTween.alphaCanvas(locationFade, 0f, 1f).setDestroyOnComplete(true);
+    }
 }
